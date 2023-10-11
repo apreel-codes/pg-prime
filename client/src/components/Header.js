d144a93de7bbdf19c0cdfc6dce550378ccb62026
Apreel-hub <83452532+Apreel-hub@users.noreply.github.com>
2023-10-11 12:11:34 +0100

    first commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="header">
+            <a href="/#brand">PG PRIME</a>
+        </div>
+    )
+}
+
+export default Header;
