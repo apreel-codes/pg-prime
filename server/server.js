e024da07c9b1d7d6655907d7dc294286a819a8e6
Apreel-hub <83452532+Apreel-hub@users.noreply.github.com>
2023-10-17 10:51:21 +0100

    build

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // require('dotenv').config();
 
 import express from 'express';
-import cors from 'cors';
+import path from 'path';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -35,7 +35,11 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
     res.status(500).send({message: err.message});
