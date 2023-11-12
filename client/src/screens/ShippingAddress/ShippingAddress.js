2431988c2d185fa32ae87483b8469fe7c96216d0
Apreel-hub <83452532+Apreel-hub@users.noreply.github.com>
2023-11-12 16:46:08 +0100

    v0.45

--- client/src/screens/ShippingAddress/ShippingAddress.js
+++ client/src/screens/ShippingAddress/ShippingAddress.js
@@ -173,7 +173,7 @@ const ShippingAddress = () => {
                                 ></textarea>
                         </Form.Group>
 
-                        <div className="billing-address">
+                        <div className="billing-address flex flex-row items-center">
                                 <input className="box w-5 h-5" type="checkbox" id="billing-same" name="billing-same" value="billing-same" />
                                 <label className="biling-same" for="billing-same"> Billing and delivery address are the same</label>
                         </div>
