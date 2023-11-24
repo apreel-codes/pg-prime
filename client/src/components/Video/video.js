3ce707bbd8fe981600f8174b6112fa38baf617e9
Apreel-hub <83452532+Apreel-hub@users.noreply.github.com>
2023-11-24 18:03:32 +0100

    v0.70

--- client/src/components/Video/video.js
+++ client/src/components/Video/video.js
@@ -6,9 +6,11 @@ const HomeVideo = () => {
     return (
         <div>
             <div className='video-wrap w-[90%] mx-auto mb-5'>
-                <video width="640" height="360"  autoplay loop muted className='video mx-auto' controls>
+                {/* <video width="640" height="360"  autoplay loop muted className='video mx-auto' controls>
                     <source src="../images/video.mp4" type="video/mp4" />
-                </video>    
+                    Your browser does not support the video tag.
+                </video>     */}
+                <img className="mx-auto" src="../images/prime.gif" />
             </div>
         </div>
         
