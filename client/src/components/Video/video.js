bef2f47ea4665edfc3de8a6900d36e3b2b2304ab
Apreel-hub <83452532+Apreel-hub@users.noreply.github.com>
2023-11-24 16:29:21 +0100

    v0.68

--- client/src/components/Video/video.js
+++ client/src/components/Video/video.js
@@ -6,7 +6,7 @@ const HomeVideo = () => {
     return (
         <div>
             <div className='video-wrap w-[90%] mx-auto mb-5'>
-                <video autoplay loop muted className='video' controls>
+                <video width="640" height="360"  autoplay loop muted className='video mx-auto' controls>
                     <source src="../images/video.mp4" type="video/mp4" />
                 </video>    
             </div>
