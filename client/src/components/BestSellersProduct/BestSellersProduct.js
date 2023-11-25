42eb74bd0d25d01349c774905e5e4a35830c0192
Apreel-hub <83452532+Apreel-hub@users.noreply.github.com>
2023-11-25 10:02:32 +0100

    v0.73

--- client/src/components/BestSellersProduct/BestSellersProduct.js
+++ client/src/components/BestSellersProduct/BestSellersProduct.js
@@ -39,7 +39,6 @@ const BestSellersProduct = (props) => {
         const existItem = cart.cartItems.find((x) => x._id === product._id);
         const quantity = existItem ? existItem.quantity + 1 : 1;
         const { data } = await axios.get(`/api/products/${product._id}`);
-        // console.log(cart.cartItems);
         if (data.countInStock < quantity ) {
             window.alert('Sorry. Product is out of stock');
             return;
